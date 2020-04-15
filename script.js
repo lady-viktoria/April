467589b2a64963d360aa9f8f86fed9d22b970f44
Vika <viktoria212@mail.ru>
2020-04-16 00:48:00 +0300

    two

--- script.js
+++ script.js
@@ -10,9 +10,9 @@
 // console.log(num);
 
 
-function calc (a,b) {
+/*function calc (a,b) {
     return (a + b);
 }
-
-console.log (calc(3,4));
+*/
+console.log (calc (3,4));
 console.log (calc (8,4));
