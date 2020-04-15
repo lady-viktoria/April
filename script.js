9d72fcc78cae51607caf3c0ed0ce23ff1f3a579b
Vika <viktoria212@mail.ru>
2020-04-16 00:43:05 +0300

    one

---
+++ script.js
@@ -0,0 +1,18 @@
+//'use strict'
+// let num = 20;
+// function showFirstMessage(text) {
+//     alert(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello World");
+// console.log(num);
+
+
+function calc (a,b) {
+    return (a + b);
+}
+
+console.log (calc(3,4));
+console.log (calc (8,4));
